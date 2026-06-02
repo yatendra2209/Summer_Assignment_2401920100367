ddb637d6d5e9ddb998e6fea81bf3fb4bfcec71a2
yatendra2209 <yatendra2209@gmail.com>
2026-06-02 21:32:11 +0530

    day 2

---
+++ week 1/day 2/Contains_Duplicate.java
@@ -0,0 +1,21 @@
+import java.util.HashSet;
+import java.util.Set;
+class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        Set<Integer> seen = new HashSet<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            if (seen.contains(nums[i])) {
+                return true;
+            }
+
+            seen.add(nums[i]);
+
+            if (seen.size() > k) {
+                seen.remove(nums[i - k]);
+            }
+        }
+
+        return false;
+    }
+}
