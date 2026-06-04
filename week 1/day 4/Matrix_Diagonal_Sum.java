2df11a33c6da4edad1aa792250ff11fca06d3aef
yatendra2209 <yatendra2209@gmail.com>
2026-06-04 22:55:26 +0530

    day 4 complet

---
+++ week 1/day 4/Matrix_Diagonal_Sum.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int diagonalSum(int[][] mat) {
+        int n=mat.length;
+        int sum=0;
+        for (int i=0;i<n;i++){
+            sum+=mat[i][i];
+            if (i!=n-1-i) {
+                sum+= mat[i][n-i-1];
+        }
+        }
+        return sum;
+    }
+}
---
+++ week 1/day 4/spiral_matrix.java
@@ -0,0 +1,42 @@
+ import java.util.List;
+ import java.util.ArrayList;
+class Solution {
+    
+    public List<Integer> spiralOrder(int[][] matrix) {
+         List<Integer> arr=new ArrayList<>();
+        int n=matrix.length;
+        int m=matrix[0].length;
+        int left=0,right =m-1,top=0,bottom=n-1;
+       
+        while (top<=bottom  && left<=right) {
+            
+        
+        for (int i=left;i<=right;i++){
+            arr.add(matrix[top][i]);        
+        
+        }
+        top++;
+        for (int i=top;i<=bottom;i++){
+            arr.add(matrix[i][right]);   
+            
+        }
+        right--;
+
+        if(top<=bottom){
+         for (int i=right;i>=left;i--){
+            arr.add(matrix[bottom][i]);
+         }
+         bottom--;
+        }
+        if (left<=right){
+         for(int i=bottom;i>=top;i--){
+           arr.add(matrix[i][left]); 
+            
+         }
+         left++;
+        }
+        }
+        return arr;
+        
+    }
+}
